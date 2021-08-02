e70312f5c77cdb12444c94d2cdab57c3df6bc92f
rod-glover <rod.glover.ca@gmail.com>
2021-08-02 10:48:57 -0700

    Improve scripts

--- supplementary/docker-group.sh
+++ supplementary/docker-group.sh
@@ -2,14 +2,30 @@
 # This script creates a group on the host that a Docker container maps to with user
 # namespace remapping enabled.
 
-host_userns_groupname=${1:-dockeragent}
-container_gid=${2:-1000}
-host_groupname_prefix=${3:-"$host_userns_groupname"}
+host_userns_groupname=dockeragent
+container_gid=1000
+host_groupname_prefix="$host_userns_groupname"
+
+while getopts "n:i:p:" flag
+do
+  case "$flag" in
+    n) host_userns_groupname="$OPTARG";;
+    i) container_gid="$OPTARG";;
+    p) host_groupname_prefix="$OPTARG";;
+    *) printf "gronk"
+       exit 1;;
+  esac
+done
 
 host_gid_base=$(egrep "^$host_userns_groupname" /etc/subgid | cut -d ':' -f 2)
+if [ -z "$host_gid_base" ]; then
+  echo "User '$host_userns_groupname' (option -n) not found in /etc/subgid."
+  exit 1
+fi
+
 host_gid=$(( host_gid_base + container_gid ))
 
 host_groupname="${host_groupname_prefix}${container_gid}"
 
 #sudo groupadd -g "$host_gid" "$host_groupname"
-echo "group=$host_groupname($host_gid)"
+echo "added host group $host_gid($host_groupname)"
--- supplementary/docker-user.sh
+++ supplementary/docker-user.sh
@@ -2,15 +2,37 @@
 # This script creates a user on the host that a Docker container maps to with user
 # namespace remapping enabled.
 
-host_groupname=${1:-dockeragent1000}
-host_userns_username=${1:-dockeragent}
-container_uid=${2:-1000}
-host_username_prefix=${3:-"$host_userns_username"}
+host_userns_username=dockeragent
+container_uid=1000
+host_username_prefix="$host_userns_username"
+host_groupname="${host_userns_username}${container_uid}"
+
+while getopts "g:n:i:p:" flag
+do
+  case "$flag" in
+    g) host_groupname="$OPTARG";;
+    n) host_userns_username="$OPTARG";;
+    i) container_uid="$OPTARG";;
+    p) host_username_prefix="$OPTARG";;
+    *) printf "gronk"
+       exit 1;;
+  esac
+done
+
+if [ ! $(getent group $host_groupname) ]; then
+  echo "Host group '$host_groupname' (option -g) does not exist."
+  exit 1
+fi
 
 host_uid_base=$(egrep "^$host_userns_username" /etc/subuid | cut -d ':' -f 2)
+if [ -z "$hostuid_base" ]; then
+  echo "User '$host_userns_username' (option -n) not found in /etc/subuid."
+  exit 1
+fi
+
 host_uid=$(( host_uid_base + container_uid ))
 
 host_username="${host_username_prefix}${container_uid}"
 
-#sudo useradd -g "$host_uid" "$host_username"
-echo "user=$host_username($host_uid) group=$host_groupname"
+#sudo useradd -g "$host_groupname" -u "$host_uid" "$host_username"
+echo "added host user $host_uid($host_username) to group $host_groupname"
